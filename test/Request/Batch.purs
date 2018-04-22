f7849de177beca756b4243b610f295827ae1b048
Matthew Leon <ml@matthewleon.com>
2018-04-21 21:33:19 -0400

    Better Batch test labeling.

--- test/Request/Batch.purs
+++ test/Request/Batch.purs
@@ -13,10 +13,11 @@ import Test.Spec (Spec, describe, it)
 import Test.Spec.QuickCheck (QCRunnerEffects, quickCheck)
 
 spec :: forall r. Spec (QCRunnerEffects r) Unit
-spec = describe "bidirectional serialization" $
-  it "roundtrips" $
-    quickCheck $
-      (gen <#> \batch -> fromJson (toJson batch) == Just (Right batch))
-        :: Gen Boolean
+spec = describe "Request.Batch" $
+  describe "bidirectional serialization" $
+    it "roundtrips" $
+      quickCheck $
+        (gen <#> \batch -> fromJson (toJson batch) == Just (Right batch))
+          :: Gen Boolean
   where
   gen = wrap <$> unfoldable genRequest
