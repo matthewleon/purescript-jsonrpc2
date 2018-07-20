729569026a82ea68d1fa476e45b63d0bb0c58a44
Matthew Leon <ml@matthewleon.com>
2018-07-19 18:58:28 -0700

    update test script

--- test.sh
+++ test.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-# exit when any command fails
-set -e
-
-./build.sh
+./build.sh || exit 1
 
 BOWER=${BOWER:-bower}
-bower install
+bower install || exit 1
 
 PURS=${PURS:-purs}
 PURS_REL_PATH='**/*.purs'
-SCRIPT_REL_PATH="src/${PURS_REL_PATH}"
-DEPS="bower_components/purescript-*/${SCRIPT_REL_PATH}"
-SRC="./${SCRIPT_REL_PATH}"
-TEST="./test/${PURS_REL_PATH}"
-${PURS} compile "${DEPS}" "${SRC}" "${TEST}"
+SCRIPT_REL_PATH="src/$PURS_REL_PATH"
+DEPS="bower_components/purescript-*/$SCRIPT_REL_PATH"
+SRC="./$SCRIPT_REL_PATH"
+TEST="./test/$PURS_REL_PATH"
+"$PURS" compile "$DEPS" "$SRC" "$TEST" || exit 1
 
 node -e 'require("./output/Test.Main/index.js").main();'
