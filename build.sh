dd6a463cb82c232ea79d8c6b297807504f5e746a
Matthew Leon <ml@matthewleon.com>
2018-07-19 18:56:44 -0700

    build script simplification

--- build.sh
+++ build.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
-# exit when any command fails
-set -e
-
 DEP_DIR="bower_components"
+
 if [ ! -d ${DEP_DIR} ]; then
   BOWER=${BOWER:-$(command -v bower)}
-  if [ -z ${BOWER} ]; then
+  if [ -z $BOWER ]; then
     >&2 echo "build failed: please install bower (https://bower.io/#install-bower)"
     exit 1
   fi
-  ${BOWER} install --production
+  $BOWER install --production || exit 1
 fi
 
 PURS=${PURS:-$(command -v purs)}
-if [ -z ${PURS} ]; then
-  >&2 echo "build failed: please install purescript (http://www.purescript.org/)"
+if [ -z $PURS ]; then
+  >&2 echo 'build failed: please install purescript (http://www.purescript.org/)'
   exit 1
 fi
 
 SCRIPT_REL_PATH='src/**/*.purs'
 DEPS="${DEP_DIR}/purescript-*/${SCRIPT_REL_PATH}"
 SRC="./${SCRIPT_REL_PATH}"
-${PURS} compile "${DEPS}" "${SRC}"
+"$PURS" compile "$DEPS" "$SRC"
